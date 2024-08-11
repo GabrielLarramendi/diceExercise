aa3331c23f9a0a2352f070b644980766b9a8b326
Larramendi <gab.larramendi@gmail.com>
2024-08-10 21:57:16 -0300

    Add same number if

--- app/src/main/java/com/larramendi/dado/MainActivity.kt
+++ app/src/main/java/com/larramendi/dado/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,10 +18,26 @@ class MainActivity : AppCompatActivity() {
         val image: ImageView = findViewById(R.id.imageView)
         val button: Button = findViewById(R.id.button)
         val text: TextView = findViewById(R.id.textView2)
+        var x = 0
 
         button.setOnClickListener {
             val value = (1..6).random()
             text.text = "Resultdo: ${value}"
+
+            when(value) {
+                1 -> image.setImageResource(R.drawable.dice1)
+                2 -> image.setImageResource(R.drawable.dice2)
+                3 -> image.setImageResource(R.drawable.dice3)
+                4 -> image.setImageResource(R.drawable.dice4)
+                5 -> image.setImageResource(R.drawable.dice5)
+                6 -> image.setImageResource(R.drawable.dice6)
+
+
+            }
+            if (value == x) {
+                Toast.makeText(this, "Número repetido", Toast.LENGTH_SHORT).show()
+            }
+            x = value
         }
     }
 }
